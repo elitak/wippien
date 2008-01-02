44cc60269babdc05a8840fba0ec16832b90fad58
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2008-01-02 14:03:04 +0000

    git-svn-id: http://www.wippien.com/svn@42 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        1,8,1,212
-#define STRFILEVER     "1, 8, 1, 212\0"
-#define MYDESCTEXTVER  "1.8.1.212"
+#define FILEVER        1,8,1,213
+#define STRFILEVER     "1, 8, 1, 213\0"
+#define MYDESCTEXTVER  "1.8.1.213"

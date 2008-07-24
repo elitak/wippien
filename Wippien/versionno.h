ff51fa93478cc2fbb86c3dc0b5f1ea4e1fdc9a52
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2008-07-24 20:56:18 +0000

    autoupdate fix
    git-svn-id: http://www.wippien.com/svn@143 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,1,2,287
-#define STRFILEVER     "2, 1, 2, 287\0"
-#define MYDESCTEXTVER  "2.1.2.287"
+#define FILEVER        2,1,2,288
+#define STRFILEVER     "2, 1, 2, 288\0"
+#define MYDESCTEXTVER  "2.1.2.288"

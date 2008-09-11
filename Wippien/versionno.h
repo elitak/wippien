98200286577c12bc8130446696135b12a213811c
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2008-09-11 17:41:56 +0000

    c node fix in wodXMPP
    git-svn-id: http://www.wippien.com/svn@213 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,1,6,330
-#define STRFILEVER     "2, 1, 6, 330\0"
-#define MYDESCTEXTVER  "2.1.6.330"
+#define FILEVER        2,1,6,331
+#define STRFILEVER     "2, 1, 6, 331\0"
+#define MYDESCTEXTVER  "2.1.6.331"

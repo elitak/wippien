33e419693e536ed5b6b844c3f76fc5b0ceca5dc8
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2008-12-08 13:19:21 +0000

    wodVPN UPNP fix - improved UPNP
    git-svn-id: http://www.wippien.com/svn@236 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,1,8,336
-#define STRFILEVER     "2, 1, 8, 336\0"
-#define MYDESCTEXTVER  "2.1.8.336"
+#define FILEVER        2,1,8,338
+#define STRFILEVER     "2, 1, 8, 338\0"
+#define MYDESCTEXTVER  "2.1.8.338"

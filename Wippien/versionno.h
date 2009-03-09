5998c034670f8fa251d2b858ae5ebc98b89945f4
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2009-03-09 20:09:31 +0000

    Join chatroom fix in wodXMPP
    git-svn-id: http://www.wippien.com/svn@261 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,2,5,365
-#define STRFILEVER     "2, 2, 5, 365\0"
-#define MYDESCTEXTVER  "2.2.5.365"
+#define FILEVER        2,2,5,366
+#define STRFILEVER     "2, 2, 5, 366\0"
+#define MYDESCTEXTVER  "2.2.5.366"

f14e4dda634fcde6d27e0d569e0ae08a0a921a8c
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2009-04-21 00:58:30 +0000

    voicechat /2
    git-svn-id: http://www.wippien.com/svn@294 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,2,6,379
-#define STRFILEVER     "2, 2, 6, 379\0"
-#define MYDESCTEXTVER  "2.2.6.379"
+#define FILEVER        2,2,6,380
+#define STRFILEVER     "2, 2, 6, 380\0"
+#define MYDESCTEXTVER  "2.2.6.380"

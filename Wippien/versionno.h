9a2b4044febe6cd5ab9ea793b0c5fece40de7a23
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2009-08-12 18:50:54 +0000

    password change fix
    git-svn-id: http://www.wippien.com/svn@324 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,2,9,391
-#define STRFILEVER     "2, 2, 9, 391\0"
-#define MYDESCTEXTVER  "2.2.9.391"
+#define FILEVER        2,2,9,392
+#define STRFILEVER     "2, 2, 9, 392\0"
+#define MYDESCTEXTVER  "2.2.9.392"

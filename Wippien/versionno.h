2a3272f8ecc3e1cbacfbc5ce3d64e1828c3bf506
kreso <kreso@d7a41678-4cf2-4d8b-951e-48bbc599225d>
2009-09-30 18:52:00 +0000

    password size on JID dialog restriction removed
    git-svn-id: http://www.wippien.com/svn@337 d7a41678-4cf2-4d8b-951e-48bbc599225d

--- Wippien/versionno.h
+++ Wippien/versionno.h
@@ -1,3 +1,3 @@
-#define FILEVER        2,3,1,398
-#define STRFILEVER     "2, 3, 1, 398\0"
-#define MYDESCTEXTVER  "2.3.1.398"
+#define FILEVER        2,3,1,399
+#define STRFILEVER     "2, 3, 1, 399\0"
+#define MYDESCTEXTVER  "2.3.1.399"
